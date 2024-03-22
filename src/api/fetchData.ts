00c5822e771a0b93b5d6bbd7f0cfbffc258888cd
sathish s <s.sathishmechddesign@gmail.com>
2024-03-22 15:53:28 +0530

    fix: secrity update

--- src/api/fetchData.ts
+++ src/api/fetchData.ts
@@ -1,19 +1,18 @@
 const fetchData = async <T>(url: string): Promise<T> => {
-	// const url = `https://api.themoviedb.org/3/trending/all/day`;
+  // const url = `https://api.themoviedb.org/3/trending/all/day`;
 
-	console.log(url);
+  console.log(url);
 
-	const options = {
-		method: 'GET',
-		headers: {
-			accept: 'application/json',
-			Authorization:
-				'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YjM1ZTQzNDFiYTU4OGU0YzZjMzI3Mzc4OTE1ZjhkYyIsInN1YiI6IjY1NmQ3YzJkNTY4NDYzMDBlZTEzN2QzMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4h6DrvVw6OSgmJ5miAU-biN8HHp1mu8drE4EiUrT6j8',
-		},
-	};
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
+    },
+  };
 
 	const res = await fetch(url, options);
-	return res.json();
+  return res.json();
 };
 
 export default fetchData;
